aaca3cb20b366b54465fa74ed9a6a2ee5e76df47
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2024-07-31 16:38:52 +0300

    Revision No. 2

--- app/src/main/java/hexlet/code/DatabaseConfig.java
+++ app/src/main/java/hexlet/code/DatabaseConfig.java
@@ -2,38 +2,31 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-
 import javax.sql.DataSource;
 
 public class DatabaseConfig {
 
-    private static final String H2_URL = "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1";
+    private static final String H2_URL = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1";
     private static final String H2_USERNAME = "sa";
     private static final String H2_PASSWORD = "";
 
-    public static DataSource getDataSource() {
-        HikariConfig config = new HikariConfig();
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        if (dbUrl == null || dbUrl.isEmpty()) {
-            dbUrl = H2_URL;
-            config.setUsername(H2_USERNAME);
-            config.setPassword(H2_PASSWORD);
-        } else {
-            System.out.println("Using JDBC_DATABASE_URL: " + dbUrl);
-            config.setUsername(System.getenv("DB_USER"));
-            config.setPassword(System.getenv("DB_PASSWORD"));
-        }
+    private static final String POSTGRES_URL = System.getenv("JDBC_DATABASE_URL");
+    private static final String POSTGRES_USERNAME = System.getenv("DB_USER");
+    private static final String POSTGRES_PASSWORD = System.getenv("DB_PASSWORD");
 
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
+    public static DataSource getDataSource() {
+        return configureDataSource(POSTGRES_URL, POSTGRES_USERNAME, POSTGRES_PASSWORD);
     }
 
     public static DataSource getTestDataSource() {
+        return configureDataSource(H2_URL, H2_USERNAME, H2_PASSWORD);
+    }
+
+    private static DataSource configureDataSource(String url, String username, String password) {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(H2_URL);
-        config.setDriverClassName("org.h2.Driver");
-        config.setUsername(H2_USERNAME);
-        config.setPassword(H2_PASSWORD);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
         return new HikariDataSource(config);
     }
 }
